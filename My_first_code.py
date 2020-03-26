4a69f11ba7272efb15f4290f759b0e7e2efce940
AWS re/Start <yemi.gbagba@hotmail.com>
2020-03-26 21:46:51 +0000

    trying new things

---
+++ My_first_code.py
@@ -0,0 +1,4 @@
+myint = 7
+print(myint)
+new_word = "The Sky Is Blue lets Have Fun"
+print(new_word)
---
+++ playcode.py
@@ -0,0 +1,5 @@
+myint = 8
+print(myint)
+new_word = "you got the hang of this"
+print(new_word)
+
